781c28ef407f5a3f4c46626691b459de080ca313
mithun24112005 <mithun24112005@gmail.com>
2025-09-30 19:37:28 +0530

    this is movie project deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // <-- change 'build' to 'dist'
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
